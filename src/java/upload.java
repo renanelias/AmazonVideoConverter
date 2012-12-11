75f24c3af6338b239b084286e4c675f06b6a34e6
Renan Elias <renan.elias@gmail.com>
2012-12-11 20:25:47 -0200

    Correção para o upload do arquivo que estava com problemas. Envio para o S3 correto.

--- src/java/upload.java
+++ src/java/upload.java
@@ -13,12 +13,21 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.ws.Response;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.FileItemFactory;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
+import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 
 /**
  *
@@ -38,56 +47,45 @@ public class upload extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String TipoRequisicao = request.getContentType();
-        if ((TipoRequisicao != null) && (TipoRequisicao.indexOf("multipart/form-data") >= 0)) {
-                DataInputStream ObjInputStream = new DataInputStream(request.getInputStream());
 
-                int TamanhoUpload = request.getContentLength();
-                byte BufferArquivo[] = new byte[TamanhoUpload];
-                int TamanhoLido = 0;
+        if (ServletFileUpload.isMultipartContent(request)) {
+            try {
+                FileItemFactory ObjFactory = new DiskFileItemFactory();
+                ServletFileUpload ObjUpload = new ServletFileUpload(ObjFactory);
+                List<FileItem> ListaItens = ObjUpload.parseRequest(request);
 
-                while (TamanhoLido < TamanhoUpload) {
-                    TamanhoLido += ObjInputStream.read(BufferArquivo, TamanhoLido, TamanhoUpload);
+                Iterator ObjIterator = ListaItens.iterator();
+                while (ObjIterator.hasNext()) {
+                    FileItem ObjItemUpload = (FileItem) ObjIterator.next();
+
+                    if (!ObjItemUpload.isFormField()) {
+                        // Cria nome do arquivo (key) para ser enviado ao servidor
+                        String NomeArquivo = ObjItemUpload.getName().substring(0,ObjItemUpload.getName().lastIndexOf("."));
+                        String ExtensaoArquivo = ObjItemUpload.getName().substring(ObjItemUpload.getName().lastIndexOf(".") + 1,ObjItemUpload.getName().length()); 
+  
+                        NomeArquivo += UUID.randomUUID().toString() + "." + ExtensaoArquivo;
+
+                        // Inicia os objetos de conexão da Amazon S3 com as credenciais
+                        AmazonS3 ObjAmazonS3 = new AmazonS3Client(new PropertiesCredentials(upload.class.getResourceAsStream("AwsCredenciais.properties")));
+                            
+                        // Envia para o servidor da Amazon
+                        ObjAmazonS3.putObject("renansbtech", NomeArquivo, ObjItemUpload.getInputStream(), null);
+
+                        // Converte o Arquivo
+                        
+                        
+                        // Redireciona o usuário para a página informando do Upload.
+                        response.sendRedirect("index.jsp");
+                    }
                 }
-                
-                InputStream ObjInputStreamEnviar = new ByteArrayInputStream(BufferArquivo);
-                String NomeVideo = UUID.randomUUID().toString();
-                
-                System.out.println(upload.class.getResourceAsStream("AwsCredenciais.properties"));
-                
-                try {
-                    AmazonS3 ObjAmazonS3 = new AmazonS3Client(new PropertiesCredentials(upload.class.getResourceAsStream("AwsCredenciais.properties")));
-
-                    PutObjectRequest PutObjectRequestCallBack = new PutObjectRequest("renansbtech", NomeVideo, ObjInputStreamEnviar, null);
-                    PutObjectRequestCallBack.setProgressListener(new ProgressListener() {
-
-                        @Override
-                        public void progressChanged(ProgressEvent progressEvent) {
-                            if (progressEvent.getEventCode() == ProgressEvent.COMPLETED_EVENT_CODE) {
-                                // Chamar Rotina para conversão dos dados...
-                                
-                            }
-                        }
-                    });
-                    ObjAmazonS3.putObject(PutObjectRequestCallBack);
-                    response.sendRedirect("index.jsp");
-                } catch (Exception e) {
-                    System.out.println("Erro"+ e.getMessage());
-                }
-                
-                
-                /*
-                FileOutputStream ObjOutputStream = new FileOutputStream(CaminhoSalvarEnviado);
-                ObjOutputStream.write(BufferArquivo);
-                ObjOutputStream.flush();
-                ObjOutputStream.close();
-        */
-
-
+            } catch (FileUploadException ex) {
+                throw new ServletException(ex.getMessage(), ex.getCause());
+            }           
+            
         } else {
-            throw new ServletException("Formulário de entrada inválido");
+            throw new ServletException("O formulário não é um upload.");
         }
+
         
     }
 
@@ -133,3 +131,68 @@ public class upload extends HttpServlet {
         return "Efetua o Download e Upload de um vídeo para o servidor da Amazon";
     }// </editor-fold>
 }
+
+
+
+
+
+        
+        
+        
+        /*
+        
+        
+        
+        
+        
+        
+        String TipoRequisicao = request.getContentType();
+        if ((TipoRequisicao != null) && (TipoRequisicao.indexOf("multipart/form-data") >= 0)) {
+                DataInputStream ObjInputStream = new DataInputStream(request.getInputStream());
+
+                int TamanhoUpload = request.getContentLength();
+                byte BufferArquivo[] = new byte[TamanhoUpload];
+                int TamanhoLido = 0;
+
+                while (TamanhoLido < TamanhoUpload) {
+                    TamanhoLido += ObjInputStream.read(BufferArquivo, TamanhoLido, TamanhoUpload);
+                }
+                
+                InputStream ObjInputStreamEnviar = new ByteArrayInputStream(BufferArquivo);
+                String NomeVideo = UUID.randomUUID().toString();
+                
+                System.out.println(upload.class.getResourceAsStream("AwsCredenciais.properties"));
+                
+                try {
+                    AmazonS3 ObjAmazonS3 = new AmazonS3Client(new PropertiesCredentials(upload.class.getResourceAsStream("AwsCredenciais.properties")));
+
+                    PutObjectRequest PutObjectRequestCallBack = new PutObjectRequest("renansbtech", NomeVideo, ObjInputStreamEnviar, null);
+                    PutObjectRequestCallBack.setProgressListener(new ProgressListener() {
+
+                        @Override
+                        public void progressChanged(ProgressEvent progressEvent) {
+                            if (progressEvent.getEventCode() == ProgressEvent.COMPLETED_EVENT_CODE) {
+                                // Chamar Rotina para conversão dos dados...
+                                
+                            }
+                        }
+                    });
+                    ObjAmazonS3.putObject(PutObjectRequestCallBack);
+                    response.sendRedirect("index.jsp");
+                } catch (Exception e) {
+                    System.out.println("Erro"+ e.getMessage());
+                }
+                
+                
+                /*
+                FileOutputStream ObjOutputStream = new FileOutputStream(CaminhoSalvarEnviado);
+                ObjOutputStream.write(BufferArquivo);
+                ObjOutputStream.flush();
+                ObjOutputStream.close();
+        * /
+
+
+        } else {
+            throw new ServletException("Formulário de entrada inválido");
+        }
+        * */
